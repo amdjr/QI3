e1008e2e43dd8dab4475d1be8273783c783058ca
amdjr <adilson_monteiro@outlook.com>
2022-03-10 04:19:41 -0300

    Comit review

--- Atividade2/src/pages/Tela2/index.js
+++ Atividade2/src/pages/Tela2/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import styles from './styleTela2Css';
 
 function Tela2() {
   return (
-    <View style={styles.bkg1}>
-      <Text style={styles.text1}>Tela 2</Text>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.bkg1}>
+        <Text style={styles.text1}>Tela 2</Text>
+        <Button title="cole" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
